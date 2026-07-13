cbc72cbab974b948839fa01b8fe2cf8348175581
Tigran <mugromell@mail.com>
2026-07-13 14:41:53 +0300

    doom

---
+++ CHAPTERS/IMP_CHAP_I.py
@@ -0,0 +1,35 @@
+#chapter1
+
+from CORE.IMPERIVM_DEFS import apply_and_show_effects, choose_event, title
+
+
+def chapter_1(player, all_chaps_data):
+    title("ГЛАВА I: Мобилизация.")
+    print("Для твоих невероятных целей тебе нужны:")
+    print("Сильная армия")
+    print("Стабильная экономика")
+    print("Хорошая дипломатия")
+
+    # ЭКОНОМИКА
+    print("\nНачнём с экономики. Она влияет на силу армии и стабильности в стране.")
+    print(f"Очки экономики: {player.economy}")
+    ec_event = choose_event(all_chaps_data["economy_events"], "экономику")
+    apply_and_show_effects(player, ec_event)
+    print("Открыта новая статистика! (3)")
+    print("АВТОРИТЕТ - показывает твой авторитет среди населения. Влияет на боевой дух, силу армии и стабильность.")
+    print("ЧЕСТЬ - показывает насколько твои действия соответствуют твоей личной философии. Влияет на авторитет.")
+    print("=" * 60)
+
+    # АРМИЯ
+    print("\nТеперь надо заняться армией. Армия высчитывается из боевого духа, снабжения, науки, авторитета, вооружения и количества солдат.")
+    print(f"Сила армии: {player.army_power}")
+    arm_event = choose_event(all_chaps_data["army_events"], "армию")
+    apply_and_show_effects(player, arm_event)
+    print("Открыта новая статистика! (1)")
+    print("СНАБЖЕНИЕ - отображает уровень логистики и провизии твоих врагов. Влияет на армию.")
+
+    # ДИПЛОМАТИЯ
+    print("\nПоследний шаг - дипломатия. Она влияет на способность создавать союзы.")
+    print(f"Очки дипломатии: {player.diplomacy}")
+    diplo_event = choose_event(all_chaps_data["diplomacy_events"], "дипломатию")
+    apply_and_show_effects(player, diplo_event)
---
+++ CHAPTERS/IMP_PRECHAP.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+# Находит папку IMPERIVM и строит путь к DATA
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ideologies_path = os.path.join(BASE_DIR, "DATA", "ideologies.json")
+philosophies_path = os.path.join(BASE_DIR, "DATA", "philosophies.json")
+
+# Загрузка данных в переменные для импорта
+with open(ideologies_path, "r", encoding="utf-8") as f:
+    ideologies = json.load(f)
+
+with open(philosophies_path, "r", encoding="utf-8") as f:
+    philosophies = json.load(f)
+
+
+
